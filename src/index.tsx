bd23c3536cbb87fc12f07e2546f7be2c99d48550
Nadav Barmatz <nadavmail@gmail.com>
2022-02-05 12:18:37 +0200

    SocketIo-client support and service

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,11 @@ import Layout from './Components/LayoutArea/Layout/Layout';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import interceptorsService from './Services/interceptorsService';
+import socketIoService from './Services/SocketIoService';
 
 interceptorsService.createInterceptors();
+
+// socketIoService.connect();
 
 ReactDOM.render(
   <React.StrictMode>
