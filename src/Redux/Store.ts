5628b399cb6697b52f01628a2f27a32e8d6e5936
Nadav Barmatz <nadavmail@gmail.com>
2022-02-13 14:02:30 +0200

    search engine fixed

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,8 +1,11 @@
 import { createStore } from "redux";
 import { authReducer } from "./AuthState";
+import { destinationReducer } from "./destinationState";
 import { userLikesReducer } from "./UserLikesState";
 import { vacationsReducer } from "./VacationsState";
 
 export const authStore = createStore(authReducer);
 export const vacationsStore = createStore(vacationsReducer);
 export const userLikesStore = createStore(userLikesReducer);
+
+export const destinationStore = createStore(destinationReducer);
