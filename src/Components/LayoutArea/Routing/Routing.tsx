3a9f7ec371fe4f0c41fdd0c36fb792af0006fb9f
Nadav Barmatz <nadavmail@gmail.com>
2022-02-13 14:05:59 +0200

    Routes has been added for chrats

--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -2,11 +2,13 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "../../AuthArea/Login/Login";
 import Logout from "../../AuthArea/Logout/Logout";
 import Register from "../../AuthArea/Register/Register";
+import LikesChart from "../../ChartsArea/LikesChart/LikesChart";
 import ContactUs from "../../ContactArea/ContactUs/ContactUs";
 import Home from "../../HomeArea/Home/Home";
 import AddVacation from "../../VacationsArea/AddVacation/AddVacation";
 import UpdateVacation from "../../VacationsArea/UpdateVacation/UpdateVacation";
 import VacationDetails from "../../VacationsArea/VacationDetails/VacationDetails";
+import VacationsByDestination from "../../VacationsArea/VacationsByDestination/VacationsByDestination";
 import VacationsList from "../../VacationsArea/VacationsList/VacationsList";
 
 function Routing(): JSX.Element {
@@ -16,6 +18,7 @@ function Routing(): JSX.Element {
                 {/* General pages  */}
                 <Route path="/home" element={<Home />} />
                 <Route path="/vacation/:id" element={<VacationDetails />} />
+                <Route path="/vacations/list-by-destination/:id" element={<VacationsByDestination />} />
                 <Route path="/contact" element={<ContactUs />} />
                 <Route path="/deals" element={<VacationsList />} />
 
@@ -27,6 +30,7 @@ function Routing(): JSX.Element {
                 {/* Admin pages */}
                 <Route path="/add-vacation" element={<AddVacation />} />
                 <Route path="/update-vacation/:id" element={<UpdateVacation />} />
+                <Route path="/charts" element={<LikesChart />} />
 
                 {/* Default page */}
                 <Route path="/*" element={<Navigate to="/home" />} />
